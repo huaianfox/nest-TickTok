97926d5b8fc85444a7c6653af49f8a9447697ee9
huaianfox <zqingl@163.com>
2019-06-27 00:34:49 +0800

    init project & first commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Post, Get } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+    ) {}
+
+  @Post('/login')
+  async login() {
+    return 'hello world';
+  }
+}
